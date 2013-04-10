67453d05e32c0f0ce572659d867690b82201d3c0
Russell Smith <qqrsmith@gmail.com>
2013-04-09 22:11:01 -0400

    Add Twilio SMS handler and rename voice handler

--- run.py
+++ run.py
@@ -3,13 +3,16 @@ import twilio.twiml
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET', 'POST'])
-def hello_monkey():
-    """Respond to incoming requests."""
+@app.route("/twilio/voice", methods=['POST'])
+def twilio_voice():
+    """Respond to incoming Twilio voice phone requests."""
     resp = twilio.twiml.Response()
     resp.say("Hello Monkey")
-
     return str(resp)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route("/twilio/text", methods=['POST'])
+def twilio_text():
+    """Respond to incoming Twilio SMS text message requests."""
+    resp = twilio.twiml.Response()
+    resp.sms("Hello Monkey")
+    return str(resp)
